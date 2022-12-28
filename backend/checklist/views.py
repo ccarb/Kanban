d858c05239606de8b7333e6b989796e323d99139
Cecilia Carbonaro <ccarbonaro@fi.uba.ar>
2022-12-28 20:13:49 -0300

    Add delete checklist endpoint

--- backend/checklist/views.py
+++ backend/checklist/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from .models import Checklist, ChecklistItem
 from .serializers import *
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 def checklists(request):
     if request.method == 'GET':
         data = Checklist.objects.all()
@@ -24,10 +24,15 @@ def checklists(request):
             return Response(status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == 'DELETE':
+        checklist = Checklist.objects.get(pk=request.GET.get('pk'))
+        checklist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['PUT', 'GET', 'DELETE'])
-def checklistItems(request, pk):
-    checklistItems = ChecklistItem.objects.filter(checklistId=pk)
+def checklistItems(request, fk):
+    checklistItems = ChecklistItem.objects.filter(checklistId=fk)
     
 
     if request.method == 'GET':
