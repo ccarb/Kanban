04e1838b902d42a3ccf59c5f17877960cd812625
Cecilia Carbonaro <ccarbonaro@fi.uba.ar>
2022-12-28 18:21:33 -0300

    refactor checklist component

--- frontend/src/components/checklists.js
+++ frontend/src/components/checklists.js
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { API_URL } from '..';
 
-class Checklists extends React.Component {
-    state={
-        checklists: []
-    };
-    getChecklists(){
-        return fetch(API_URL).then(response => response.json()).then(data => this.setState({checklists: data}));
+function Checklists(props){
+    const [checklists, setChecklists] = useState([]);
+
+    function getChecklists(){
+        return fetch(API_URL).then(response => response.json()).then(data => setChecklists(data));
     }
-    resetState(){
-        this.getChecklists();
-    }
-    componentDidMount(){
-        this.resetState();
-    }
-    render(){
-        return (
-                <div className="d-container-fluid d-sm-block card mt-3 mx-3" style={{width: "460px"}}>
-                    <div className="card-header">
-                        <p className="card-title h3">Checklists</p>
-                    </div>
-                    <div className="card-body">
-                        <ul className="list-group">
-                            {this.state.checklists.map(checklist => (<li key={checklist.pk} className='list-group-item'>{checklist.name} <span className="float-end"><i className="bi bi-trash3-fill"></i></span> </li>))}
-                        </ul>
-                    </div>
-                </div>
-          );
-    }
+
+    useEffect(()=>{
+        getChecklists();
+    },[]);
+
+    return(
+        <div className="d-container-fluid d-sm-block card mt-3 mx-3" style={{width: "460px"}}>
+            <div className="card-header">
+                <p className="card-title h3">Checklists</p>
+            </div>
+            <div className="card-body">
+                <ul className="list-group">
+                    {checklists.map(checklist => (<li key={checklist.pk} className='list-group-item'>{checklist.name} <span className="float-end"><i className="bi bi-trash3-fill"></i></span> </li>))}
+                </ul>
+            </div>
+        </div>
+    );
+
 }
 
 export default Checklists;
