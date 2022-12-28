ad3fbeb16dbdd7bb6dba94087d9ff8f852888057
Cecilia Carbonaro <ccarbonaro@fi.uba.ar>
2022-12-28 19:09:51 -0300

    add remove checklist modal

--- frontend/src/components/checklists.js
+++ frontend/src/components/checklists.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 import { API_URL } from '..';
 
 function Checklists(props){
@@ -12,17 +15,42 @@ function Checklists(props){
         getChecklists();
     },[]);
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleRemove = () => {
+        //TODO delete checklist fetch request
+        setShow(false);
+    };
+
     return(
-        <div className="d-container-fluid d-sm-block card mt-3 mx-3" style={{width: "460px"}}>
-            <div className="card-header">
-                <p className="card-title h3">Checklists</p>
+        <>
+            <div className="d-container-fluid d-sm-block card mt-3 mx-3" style={{width: "460px"}}>
+                <div className="card-header">
+                    <p className="card-title h3">Checklists</p>
+                </div>
+                <div className="card-body">
+                    <ul className="list-group">
+                        {checklists.map(checklist => (<li key={checklist.pk} className='list-group-item'>{checklist.name} <span className="float-end" onClick={handleShow}><i className="bi bi-trash3-fill"></i></span> </li>))}
+                    </ul>
+                </div>
             </div>
-            <div className="card-body">
-                <ul className="list-group">
-                    {checklists.map(checklist => (<li key={checklist.pk} className='list-group-item'>{checklist.name} <span className="float-end"><i className="bi bi-trash3-fill"></i></span> </li>))}
-                </ul>
-            </div>
-        </div>
+            <Modal show={show} onHide={handleClose} className='modal-sm'>
+                <Modal.Header closeButton>
+                    <Modal.Title>Erase checklist?</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        No
+                    </Button>
+                    <Button variant="primary" onClick={handleRemove}>
+                        Yes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 
 }
