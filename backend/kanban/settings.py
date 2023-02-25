a261b90ee6aafc0f773b6b720c334a4e692ac171
Cecilia Carbonaro <ccarbonaro@fi.uba.ar>
2023-02-25 20:38:23 -0300

    Update settings.py

--- backend/kanban/settings.py
+++ backend/kanban/settings.py
@@ -46,16 +46,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ( 'http://127.0.0.1:8000', 'http://127.0.0.1:3000','http://localhost:3000','http://localhost:8000','http://ccarb-sandbox.click')
+CORS_ALLOWED_ORIGINS = ( 'http://127.0.0.1:8000', 'http://127.0.0.1:3000','http://localhost:3000','http://localhost:8000', 'http://127.0.0.1','http://localhost',
+                         'http://ccarb-sandbox.click')
 
 ROOT_URLCONF = 'kanban.urls'
 
