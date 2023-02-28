ea1ed5dc092503424599162548946ce44eaa55ab
Cecilia Carbonaro <ccarbonaro@fi.uba.ar>
2023-02-28 19:19:48 -0300

    Create header component

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './home.css';
-import mySVG from '../assets/HomeBackground.svg';
+import background from '../assets/HomeBackground.svg';
+import {ReactComponent as HeaderLargeImg} from '../assets/HomeHeader.svg';
 
 
 function Home(props){
     const styles = {
-        backgroundImage: `url(${mySVG})`,
+        backgroundImage: `url(${background})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         position: 'absolute',
@@ -15,11 +16,34 @@ function Home(props){
         height: '100%'
     };
 
+    function Header(props){
+        return(
+            <header  className="sticky-top" style={{height:'33%'}}>
+                <div className='d-block d-sm-none' style={{position:'absolute', width:'100%'}}>
+                    <HeaderLargeImg width='100%' height='100%' viewBox='0 0 800 325'/>
+                </div>
+                <div className='d-none d-sm-block d-md-none' style={{position:'absolute', width:'100%'}}>
+                    <HeaderLargeImg width='100%' height='100%' viewBox='0 0 1400 325'/>
+                </div>
+                <div className='d-none d-md-block d-xl-none' style={{position:'absolute', width:'100%'}}>
+                    <HeaderLargeImg width='100%' height='100%'/>
+                </div>
+                <div className='d-none d-xl-block' style={{position:'absolute', width:'100%'}}>
+                    <HeaderLargeImg width='100%' height='100%' viewBox='0 100 1400 325'/>
+                </div>
+                <div style={{position:'relative', left:'2em', top:'1em'}}>
+                    <h1 className='text-primary'>Cecilia's Sandbox</h1>
+                    <h4>Contact me</h4>
+                </div>
+            </header>
+        )
+    }
+
     return (
         <>
             <div style={styles}>
+                <Header/>
                 <div id="body" className='container mt-3 mx-3'>
-                    <h1>Cecilia's Sandbox</h1>
                     <h2>Welcome! This site maps my journey to become a professional web developer. </h2>
                     <p>You can see the current milestone clicking the cross on the left.</p>
                     <p>You can also check previous milestones scrolling down.</p>
