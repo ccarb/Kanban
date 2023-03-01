18da6f210c31a2e7d2228d93f190442a82001a7c
Cecilia Carbonaro <ccarbonaro@fi.uba.ar>
2023-02-28 21:17:56 -0300

    hompage finished

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -2,23 +2,20 @@ import React from 'react';
 import './home.css';
 import background from '../assets/HomeBackground.svg';
 import {ReactComponent as HeaderLargeImg} from '../assets/HomeHeader.svg';
+import {ReactComponent as MapPath} from '../assets/MapPath.svg';
+import { Link } from 'react-router-dom';
 
 
 function Home(props){
     const styles = {
         backgroundImage: `url(${background})`,
         backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        position: 'absolute',
-        top: '0 px',
-        left: '0 px',        
-        width: '100%',
-        height: '100%'
+        backgroundPosition: 'left',
     };
 
     function Header(props){
         return(
-            <header  className="sticky-top" style={{height:'33%'}}>
+            <header  className="sticky-top" style={{height:'10em'}}>
                 <div className='d-block d-sm-none' style={{position:'absolute', width:'100%'}}>
                     <HeaderLargeImg width='100%' height='100%' viewBox='0 0 800 325'/>
                 </div>
@@ -40,16 +37,29 @@ function Home(props){
     }
 
     return (
-        <>
-            <div style={styles}>
-                <Header/>
-                <div id="body" className='container mt-3 mx-3'>
-                    <h2>Welcome! This site maps my journey to become a professional web developer. </h2>
-                    <p>You can see the current milestone clicking the cross on the left.</p>
-                    <p>You can also check previous milestones scrolling down.</p>
-                </div>
+        <div className='container-fluid h-100 p-0' style={styles}>
+            <Header/>
+            <div id="intro" className='mt-3 mx-3'>
+                <h2>Welcome! This site maps my journey to become a professional web developer. </h2>
+                <p>You can check previous milestones scrolling down.</p>
             </div>
-        </>
+            <div className='container'>
+                <MapPath width='20%' height='100%' class='d-none d-md-block float-start align-top mt-3 ' />
+                <MapPath width='39%' height='100%' class='d-block d-md-none float-start align-top mt-3 ' />
+                <span id="x-marks-the-spot" className='d-none d-md-block w-75 align-top float-end my-3 mx-3'>
+                    <h3>With pictures of the Kanban site fresh in my mind, I continue forward. I arrive at a checklist app... </h3>
+                    <div className='w-100 text-center'>
+                        <Link to="/checklists" className="btn btn-primary">Take me there</Link>
+                    </div>
+                </span>
+                <span id="x-marks-the-spot" className='d-block d-md-none w-50 align-top float-end mt-3 mx-3'>
+                    With pictures of the Kanban site fresh in my mind, I continue forward. I arrive at a checklist app...
+                    <div className='w-100 text-center mt-1'>
+                        <Link to="/checklists" className="btn btn-primary">Take me there</Link>
+                    </div>
+                </span>
+            </div>
+        </div>
     )
 }
 
