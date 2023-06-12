06cb7526e161f7dd2bb83deff915a7330e9742a5
Cecilia Carbonaro <60666995+ccarb@users.noreply.github.com>
2023-06-12 15:09:24 -0300

    modify settings to connect to RDS (#75)

--- backend/kanban/settings.py
+++ backend/kanban/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'kanban.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": config("DB_EP"),
+        "PORT": config("DB_PT", default=5432, cast=int),
+        "NAME": "postgres",
+        "USER": config("DB_US"),
+        "PASSWORD": config("DB_PW")
     }
 }
 
